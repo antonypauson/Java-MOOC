a201c679b89ee8b64c541a04c05937fdfc9f3790
Antony <13antonysun@gmail.com>
2025-01-03 18:37:25 +0530

    word

---
+++ Part2/src/Word.java
@@ -0,0 +1,10 @@
+public class Word {
+    public static void main(String[] args) {
+        String myString = word();
+        System.out.println(myString);
+    }
+
+    public static String word() {
+        return "My man is improving, small steps!";
+    }
+}
