4fbc2acb11b210c8a7d1ff2845cf719d8855e1ed
Antony <13antonysun@gmail.com>
2025-01-03 18:35:55 +0530

    number uno

---
+++ Part2/src/NumberUno.java
@@ -0,0 +1,10 @@
+public class NumberUno {
+    public static void main(String[] args) {
+        int num = numberUno();
+        System.out.println(num);
+    }
+
+    public static int numberUno() {
+        return 1;
+    }
+}
