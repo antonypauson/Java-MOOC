16d3b0bc1894599fe615a9bbabd6c028da125296
Antony <13antonysun@gmail.com>
2025-01-05 12:45:24 +0530

    whistle

---
+++ Part4/IntroToOOP/src/Room.java
@@ -0,0 +1,10 @@
+public class Room {
+    //creating a constructor for this class
+    private String code;
+    private int seats;
+
+    public Room(String classCode, int numberOfSeats) {
+        this.code = classCode;
+        this.seats = numberOfSeats;
+    }
+}
