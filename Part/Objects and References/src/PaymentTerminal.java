44b6abe5d7fd7c71e0915d1c36643d16bfa805b7
Antony <13antonysun@gmail.com>
2025-01-09 00:52:26 +0530

    payments card part 4

--- Part/Objects and References/src/PaymentTerminal.java
+++ Part/Objects and References/src/PaymentTerminal.java
@@ -49,7 +49,9 @@ public class PaymentTerminal {
         return false;
     }
 
-
+    public void addMoneyToCard(PaymentCard card, double sum) {
+        card.addMoney(sum);
+    }
 
     public String toString() {
         return "money: " + money + ", number of sold afforable meals: " + affordableMeals + ", number of sold hearty meals: " + heartyMeals;
@@ -58,8 +60,8 @@ public class PaymentTerminal {
     public static void main(String[] args) {
         PaymentTerminal unicafeExactum = new PaymentTerminal();
 
-        double change = unicafeExactum.eatAffordably(10);
-        System.out.println("remaining change " + change);
+//        double change = unicafeExactum.eatAffordably(10);
+//        System.out.println("remaining change " + change);
 
 //        change = unicafeExactum.eatAffordably(5);
 //        System.out.println("remaining change " + change);
@@ -69,16 +71,35 @@ public class PaymentTerminal {
 //
 //        System.out.println(unicafeExactum);
 
-        PaymentCard annesCard = new PaymentCard(7);
+//        PaymentCard annesCard = new PaymentCard(7);
+//
+//        boolean wasSuccessful = unicafeExactum.eatHeartily(annesCard);
+//        System.out.println("there was enough money: " + wasSuccessful);
+//
+//        wasSuccessful = unicafeExactum.eatHeartily(annesCard);
+//        System.out.println("there was enough money: " + wasSuccessful);
+//
+//        wasSuccessful = unicafeExactum.eatAffordably(annesCard);
+//        System.out.println("there was enough money: " + wasSuccessful);
+//
+//        System.out.println(unicafeExactum);
+
+
+        System.out.println(unicafeExactum);
+
+        PaymentCard annesCard = new PaymentCard(2);
+
+        System.out.println("amount of money on the card is " + annesCard.balance() + " euros");
 
         boolean wasSuccessful = unicafeExactum.eatHeartily(annesCard);
         System.out.println("there was enough money: " + wasSuccessful);
 
+        unicafeExactum.addMoneyToCard(annesCard, 100);
+
         wasSuccessful = unicafeExactum.eatHeartily(annesCard);
         System.out.println("there was enough money: " + wasSuccessful);
 
-        wasSuccessful = unicafeExactum.eatAffordably(annesCard);
-        System.out.println("there was enough money: " + wasSuccessful);
+        System.out.println("amount of money on the card is " + annesCard.balance() + " euros");
 
         System.out.println(unicafeExactum);
     }
