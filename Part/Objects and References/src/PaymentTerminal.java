0e327a4dbc07c57e532205ac453b27b592a636e8
Antony <13antonysun@gmail.com>
2025-01-09 00:39:39 +0530

    payments card part 2

---
+++ Part/Objects and References/src/PaymentTerminal.java
@@ -0,0 +1,48 @@
+public class PaymentTerminal {
+    private double money;
+    private int affordableMeals;
+    private int heartyMeals;
+
+    public PaymentTerminal() {
+        this.money = 1000;
+    }
+
+    public double eatAffordably(double payment) {
+        double cost = 2.50;
+        if (payment >= cost) {
+            this.money += cost;
+            affordableMeals++;
+            return payment - cost;
+        }
+        return payment;
+    }
+
+    public double eatHeartily(double payment) {
+        double cost = 4.30;
+        if (payment >= cost) {
+            this.money += cost;
+            heartyMeals++;
+            return payment - cost;
+        }
+        return payment;
+    }
+
+    public String toString() {
+        return "money: " + money + ", number of sold afforable meals: " + affordableMeals + ", number of sold hearty meals: " + heartyMeals;
+    }
+
+    public static void main(String[] args) {
+        PaymentTerminal unicafeExactum = new PaymentTerminal();
+
+        double change = unicafeExactum.eatAffordably(10);
+        System.out.println("remaining change " + change);
+
+        change = unicafeExactum.eatAffordably(5);
+        System.out.println("remaining change " + change);
+
+        change = unicafeExactum.eatHeartily(4.3);
+        System.out.println("remaining change " + change);
+
+        System.out.println(unicafeExactum);
+    }
+}
