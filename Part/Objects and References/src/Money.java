9da4cb3f327a7740fd9d23f7d0c7bcc4b51c3fbd
Antony <13antonysun@gmail.com>
2025-01-10 16:13:42 +0530

    money

---
+++ Part/Objects and References/src/Money.java
@@ -0,0 +1,58 @@
+public class Money {
+
+    private final int euros;
+    private final int cents;
+
+    public Money(int euros, int cents) {
+        this.euros = euros;
+        this.cents = cents;
+    }
+
+    public int euros() {
+        return euros;
+    }
+
+    public int cents() {
+        return cents;
+    }
+
+    public String toString() {
+        String zero = "";
+        if (cents <= 10) {
+            zero = "0";
+        }
+
+        return euros + "." + zero + cents + "e";
+    }
+
+    public Money plus(Money addition) {
+        Money addedMoney = new Money(this.euros + addition.euros, this.cents + addition.cents);
+        return addedMoney;
+    }
+
+    public Money minus(Money decreaser) {
+        Money reducedMoney = new Money(this.euros. - decreaser.euros, this.cents - decreaser.cents);
+        return reducedMoney;
+    }
+
+    public boolean lessThan(Money compared) {
+        return this.euros < compared.euros;
+    }
+
+    public static void main(String[] args) {
+        Money a = new Money(10, 0);
+        Money b = new Money(5, 0);
+        System.out.println(a);
+        System.out.println(b);
+
+        Money c = a.plus(b);
+        System.out.println(c);
+
+        a = a.plus(c);
+        System.out.println(a);
+
+        System.out.println(a.lessThan(b));
+
+
+    }
+}
