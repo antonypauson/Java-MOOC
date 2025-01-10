3ccc5a1a930952e47cdc0baa0718b305b5a0e577
Antony <13antonysun@gmail.com>
2025-01-10 16:46:33 +0530

    stack

--- Part6/Chapter1/src/Stack.java
+++ Part6/Chapter1/src/Stack.java
@@ -33,6 +33,17 @@ public class Stack {
         System.out.println(s.values());
         String taken = s.take();
         System.out.println(s.values());
+        s.take();
+
+        s.add("1");
+        s.add("2");
+        s.add("3");
+        s.add("4");
+        s.add("5");
+
+        while (!s.isEmpty()) {
+            System.out.println(s.take());
+        }
     }
 
 }
