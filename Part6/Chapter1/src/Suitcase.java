d24cb47e0e29490d785432c10c10ec08a98d1ca6
Antony <13antonysun@gmail.com>
2025-01-11 13:32:19 +0530

    cargo part 4

--- Part6/Chapter1/src/Suitcase.java
+++ Part6/Chapter1/src/Suitcase.java
@@ -24,7 +24,7 @@ public class Suitcase {
     }
 
     public String toString() {
-        if (items.size() < 1) {
+        if (items.isEmpty()) {
             return "no items (0 kg)";
         }
 
@@ -32,6 +32,19 @@ public class Suitcase {
             return items.size() + " item (" + totalWeight + " kg)";
         }
         return items.size() + " items (" + totalWeight + " kg)";
+    }
+
+    public void printItems() {
+        String output = "The suitcase contains the following items\n";
+
+        for (Item item : items) {
+            output += item.toString() + "\n";
+        }
+        System.out.println(output);
+    }
+
+    public int getTotalWeight() {
+        return totalWeight;
     }
 
     public static void main(String[] args) {
@@ -47,5 +60,8 @@ public class Suitcase {
         System.out.println(suitcase);
         suitcase.addItem(brick);
         System.out.println(suitcase);
+
+        suitcase.printItems();
+        System.out.println("Total weight: " + suitcase.getTotalWeight() + " kg");
     }
 }
