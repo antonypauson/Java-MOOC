74f7757a8fa0a7acc433be275159ab7acb900f22
Antony <13antonysun@gmail.com>
2025-01-12 20:38:53 +0530

    Sorting Part 2

--- Part7/Lesson2/src/MainProgram.java
+++ Part7/Lesson2/src/MainProgram.java
@@ -10,8 +10,20 @@ public class MainProgram {
         }
         return smallest;
     }
+
+    public static int indexOfSmallest(int[] array) {
+        int smallest = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (smallest > array[i]) {
+                smallest = array[i];
+                return i;
+            }
+        }
+        return 0;
+    }
     public static void main(String[] args) {
         int[] numbers = {6, 5, 8, 7, 11};
         System.out.println("Smallest: " + MainProgram.smallest(numbers));
+        System.out.println("Index of the smallest number: " + MainProgram.indexOfSmallest(numbers));
     }
 }
