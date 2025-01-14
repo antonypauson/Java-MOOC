44f11978f6f2778f237dc2e64f29a4539ecac0d4
Antony <13antonysun@gmail.com>
2025-01-14 21:26:32 +0530

    searching part 2

--- Part7/Lesson2/Searching/src/Main.java
+++ Part7/Lesson2/Searching/src/Main.java
@@ -32,6 +32,28 @@ public class Main {
         return books;
     }
 
+    public static int binarySearch(ArrayList<Book> books, int searchId) {
+        int begin = 0;
+        int end = books.size() - 1;
+
+        while (begin < end) {
+            int middle = (begin + end) / 2;
+            if (books.get(middle).getId() == searchId) {
+                return middle;
+            }
+
+            if (books.get(middle).getId() < searchId) {
+                begin = middle++;
+            }
+
+            if (books.get(middle).getId() > searchId) {
+                end = middle--;
+            }
+
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
         Main books = new Main();
         books.add(new Book (101,"First book"));
@@ -42,5 +64,6 @@ public class Main {
         System.out.println(books);
 
         System.out.println(Main.linearSearch(books.getBooks(), 103));
+        System.out.println(Main.binarySearch(books.getBooks(), 103));
     }
 }
