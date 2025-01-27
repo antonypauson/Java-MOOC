f8e76e2d16e5a9c787603e6a24b2047fd7d48de8
Antony <13antonysun@gmail.com>
2025-01-27 22:20:36 +0530

    same date

---
+++ Part8/Chapter 3/Same Date/src/SimpleDate.java
@@ -0,0 +1,32 @@
+public class SimpleDate {
+    private int day, month, year;
+
+    public SimpleDate(int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+
+        if (!(object instanceof SimpleDate)) {
+            return false;
+        }
+
+        SimpleDate dateObject = (SimpleDate) object;
+
+        return (dateObject.day == this.day &&
+                dateObject.month == this.month &&
+                dateObject.year == this.year);
+    }
+
+    public static void main(String[] args) {
+        SimpleDate d = new SimpleDate(1, 2, 2000);
+        System.out.println(d.equals("heh"));
+        System.out.println(d.equals(new SimpleDate(5, 2, 2012)));
+        System.out.println(d.equals(new SimpleDate(1, 2, 2000)));
+    }
+}
