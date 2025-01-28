2323cd385a664de5ead4a9ee81fb0accdeebacba
Antony <13antonysun@gmail.com>
2025-01-28 13:14:36 +0530

    Person and subclasses part 1

---
+++ Part9/Chapter One/Super Sub Class/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Superclass sup = new Superclass();
+        Subclass sub = new Subclass();
+
+        System.out.println(sup);
+        System.out.println(sub);
+    }
+}
