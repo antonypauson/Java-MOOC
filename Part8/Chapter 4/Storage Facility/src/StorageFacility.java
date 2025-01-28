5a36210f95e2854cd5d577311a9de9433d79c17e
Antony <13antonysun@gmail.com>
2025-01-28 11:56:58 +0530

    storage facility part 1

---
+++ Part8/Chapter 4/Storage Facility/src/StorageFacility.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class StorageFacility {
+    private HashMap<String, ArrayList<String>> storage;
+
+    public StorageFacility() {
+        storage = new HashMap<>();
+    }
+
+    public void add(String unit, String item) {
+        this.storage.putIfAbsent(unit, new ArrayList<>());
+        this.storage.get(unit).add(item);
+    }
+
+    public ArrayList<String> contents(String storageUnit) {
+        return this.storage.get(storageUnit);
+    }
+
+    public static void main(String[] args) {
+        StorageFacility facility = new StorageFacility();
+        facility.add("a14", "ice skates");
+        facility.add("a14", "ice hockey stick");
+        facility.add("a14", "ice skates");
+
+        facility.add("f156", "rollerblades");
+        facility.add("f156", "rollerblades");
+
+        facility.add("g63", "six");
+        facility.add("g63", "pi");
+
+        System.out.println(facility.contents("a14"));
+        System.out.println(facility.contents("f156"));
+    }
+}
