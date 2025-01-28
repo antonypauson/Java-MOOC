2be448f2618076f8801529427e8b3cfa6494f993
Antony <13antonysun@gmail.com>
2025-01-28 13:20:26 +0530

    Person and subclasses part 2

--- Part9/Chapter One/Person/src/Student.java
+++ Part9/Chapter One/Person/src/Student.java
@@ -1,2 +1,25 @@
-public class Student {
+public class Student extends Person {
+    private int credit;
+
+    public Student(String name, String address) {
+        super(name, address);
+        this.credit = 0;
+    }
+
+    public String credits() {
+        return "Study credits " + credit;
+    }
+
+    public void study() {
+        this.credit++;
+    }
+
+    public static void main(String[] args) {
+        Student ollie = new Student("Ollie", "Ollie's Address");
+        System.out.println(ollie);
+        System.out.println(ollie.credits());
+        ollie.study();
+        System.out.println(ollie.credits());
+    }
+
 }
