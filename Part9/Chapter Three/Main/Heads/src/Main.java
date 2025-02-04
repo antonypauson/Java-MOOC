7758b22ecfd562e6e58cd753a822582366dae206
Antony <13antonysun@gmail.com>
2025-02-05 00:29:28 +0530

    herds part 2

--- Part9/Chapter Three/Main/Heads/src/Main.java
+++ Part9/Chapter Three/Main/Heads/src/Main.java
@@ -6,5 +6,13 @@ public class Main {
         System.out.println(organism);
         organism.move(50, 20);
         System.out.println(organism);
+
+        Herd herd = new Herd();
+        herd.addToHerd(new Organism(57,66));
+        herd.addToHerd(new Organism(73, 56));
+        herd.addToHerd(new Organism(46, 52));
+        herd.addToHerd(new Organism(19, 107));
+
+        System.out.println(herd);
     }
 }
